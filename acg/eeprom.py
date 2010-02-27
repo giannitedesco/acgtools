7d156f73eb4bd4702188663fe6f6a79ecdd0a26f
Gianni Tedesco <gianni@scaramanga.co.uk>
2010-02-27 11:10:31 +0000

    Command line EEPROM modification tool

--- acg/eeprom.py
+++ acg/eeprom.py
@@ -45,15 +45,19 @@ class eeprom:
 		return str(self.__bin)
 	
 	def get_dev_id(self):
+		"Device ID"
 		return int("".join(map(lambda x:"%.2x"%x,
 					self.__bin[0:4])), 16)
 	def set_dev_id(self, dev_id):
+		"Device ID"
 		raise ACG_EEPROM_ValueError("Device ID is read-only")
 
 	def get_admin_data(self):
+		"Admin data"
 		return int("".join(map(lambda x:"%.2x"%x,
 					self.__bin[4:8])), 16)
 	def set_admin_data(self, dev_id):
+		"Admin data"
 		raise ACG_EEPROM_ValueError("Admin data is read-only")
 
 	def __set_byte(self, ofs, byte):
@@ -63,21 +67,27 @@ class eeprom:
 		self.__bin[ofs] = byte
 
 	def get_station_id(self):
+		"Station ID"
 		return self.__bin[0x0a]
 	def set_station_id(self, s_id):
+		"Station ID"
 		self.__set_byte(0x0a, s_id)
 	
 	def get_baud_rate(self):
+		"Baud Rate"
 		baud = self.__bin[0x0c] & 7
 		return baud_rates[baud]
 	def set_baud_rate(self, rate):
+		"Baud Rate"
 		if rate < 0 or not rate in baud_rates:
 			raise ACG_EEPROM_ValueError("Invalid baud rate")
 		self.__bin[0x0c] = chr(baud_rates.index(rate))
 
 	def get_guard(self):
+		"Guart tome (ms)"
 		return (37.8 * float(self.__bin[0xd])) / 1000.0
 	def set_guard(self, guard):
+		"Guart tome (ms)"
 		try:
 			g = float(guard)
 		except:
@@ -98,83 +108,115 @@ class eeprom:
 			self.__bin[byte] &= (~bit) & 0xff
 
 	def get_auto_start(self):
+		"Auto start"
 		return self.__get_bit(0x0b, PCON1_AUTO_START)
 	def set_auto_start(self, bit):
+		"Auto start"
 		self.__set_bit(0xb, PCON1_AUTO_START, bit)
 
 	def get_binary(self):
+		"Binary protocol"
 		return self.__get_bit(0x0b, PCON1_PROTOCOL)
 	def set_binary(self, bit):
+		"Binary protocol"
 		self.__set_bit(0xb, PCON1_PROTOCOL, bit)
 
 	def get_multitag(self):
+		"Multitag"
 		return self.__get_bit(0x0b, PCON1_MULTITAG)
 	def set_multitag(self, bit):
+		"Multitag"
 		self.__set_bit(0xb, PCON1_MULTITAG, bit)
 
 	def get_new_serial_mode(self):
+		"New serial mode"
 		return self.__get_bit(0x0b, PCON1_NEW_SERIAL_MODE)
 	def set_new_serial_mode(self, bit):
+		"New serial mode"
 		self.__set_bit(0xb, PCON1_NEW_SERIAL_MODE, bit)
 
 	def get_program_led(self):
+		"Programmed LEDs"
 		return self.__get_bit(0x0b, PCON1_LED)
 	def set_program_led(self, bit):
+		"Programmed LEDs"
 		self.__set_bit(0xb, PCON1_LED, bit)
 
 	def get_single_shot(self):
+		"Single shot mode"
 		return self.__get_bit(0x0b, PCON1_SINGLE_SHOT)
 	def set_single_shot(self, bit):
+		"Single shot mode"
 		self.__set_bit(0xb, PCON1_SINGLE_SHOT, bit)
 
 	def get_ext_protocol(self):
+		"Ext. protocol"
 		return self.__get_bit(0x0b, PCON1_EXT_PROTOCOL)
 	def set_ext_protocol(self, bit):
+		"Ext. protocol"
 		self.__set_bit(0xb, PCON1_EXT_PROTOCOL, bit)
 
 	def get_ext_id(self):
+		"Ext. ID's"
 		return self.__get_bit(0x0b, PCON1_EXT_ID)
 	def set_ext_id(self, bit):
+		"Ext. ID's"
 		self.__set_bit(0xb, PCON1_EXT_ID, bit)
 
 	def get_op_1443a(self):
+		"op: ISO 1443-A"
 		return self.__get_bit(0x0e, OPMODE_1443A)
 	def set_op_1443a(self, bit):
+		"op: ISO 1443-A"
 		self.__set_bit(0xe, OPMODE_1443A, bit)
 
 	def get_op_1443b(self):
+		"op: ISO 1443-B"
 		return self.__get_bit(0x0e, OPMODE_1443B)
 	def set_op_1443b(self, bit):
+		"op: ISO 1443-B"
 		self.__set_bit(0xe, OPMODE_1443B, bit)
 
 	def get_op_sr176(self):
+		"op: SR-176"
 		return self.__get_bit(0x0e, OPMODE_SR176)
 	def set_op_sr176(self, bit):
+		"op: SR-176"
 		self.__set_bit(0xe, OPMODE_SR176, bit)
 
 	def get_op_icode(self):
+		"op: ICODE"
 		return self.__get_bit(0x0e, OPMODE_ICODE)
 	def set_op_icode(self, bit):
+		"op: ICODE"
 		self.__set_bit(0xe, OPMODE_ICODE, bit)
 
 	def get_op_15693(self):
+		"op: ISO 15693"
 		return self.__get_bit(0x0e, OPMODE_15693)
 	def set_op_15693(self, bit):
+		"op: ISO 15693"
 		self.__set_bit(0xe, OPMODE_15693, bit)
 
 	def get_op_icode_epc(self):
+		"op: ICODE-EPC"
 		return self.__get_bit(0x0e, OPMODE_ICODE_EPC)
 	def set_op_icode_epc(self, bit):
+		"op: ICODE-EPC"
 		self.__set_bit(0xe, OPMODE_ICODE_EPC, bit)
 
 	def get_op_icode_uid(self):
+		"op: ICODE-UID"
 		return self.__get_bit(0x0e, OPMODE_ICODE_UID)
 	def set_op_icode_uid(self, bit):
+		"op: ICODE-UID"
 		self.__set_bit(0xe, OPMODE_ICODE_UID, bit)
 	
 	def get_single_shot_tmo(self):
+		"Single shot (ms)"
 		return self.__bin[0x0f] * 100
 	def set_single_shot_tmo(self, ms):
+		"Single shot (ms)"
 		try:
 			val = int(ms)
 		except ValueErrror:
@@ -182,46 +224,62 @@ class eeprom:
 		self.__bin[0x0f] = val / 100
 
 	def get_multitag_reset(self):
+		"Multitag reset"
 		return not self.__get_bit(0x13, PCON2_MULTITAG_RESET)
 	def set_multitag_reset(self, bit):
+		"Multitag reset"
 		self.__set_bit(0x13, PCON2_MULTITAG_RESET, not bit)
 
 	def get_startup_msg(self):
+		"Startup message"
 		return not self.__get_bit(0x13, PCON2_STARTUP_MSG)
 	def set_startup_msg(self, bit):
+		"Startup message"
 		self.__set_bit(0x13, PCON2_STARTUP_MSG, not bit)
 
 	def get_binary_frame2(self):
+		"Binary frames v2"
 		return self.__get_bit(0x13, PCON2_BINARY_FRAME2)
 	def set_binary_frame2(self, bit):
+		"Binary frames v2"
 		self.__set_bit(0x13, PCON2_BINARY_FRAME2, bit)
 
 	def get_noisy_env(self):
+		"Noisy environment"
 		return self.__get_bit(0x13, PCON2_NOISY_ENV)
 	def set_noisy_env(self, bit):
+		"Noisy environment"
 		self.__set_bit(0x13, PCON2_NOISY_ENV, bit)
 
 	def get_anti_collision(self):
+		"ISO Anti-collision"
 		return self.__get_bit(0x13, PCON2_ANTI_COLLISION)
 	def set_anti_collision(self, bit):
+		"ISO Anti-collision"
 		self.__set_bit(0x13, PCON2_ANTI_COLLISION, bit)
 
 	def get_err_handling(self):
+		"ISO Err handling"
 		return not self.__get_bit(0x13, PCON2_ERR_HANDLING)
 	def set_err_handling(self, bit):
+		"ISO Err handling"
 		self.__set_bit(0x13, PCON2_ERR_HANDLING, not bit)
 
 	def get_reset_time(self):
+		"Reset time (ms)"
 		return self.__bin[0x14]
 	def set_reset_time(self, ms):
+		"Reset time (ms)"
 		self.__set_byte(0x14, ms)
 
 	def get_recover_time(self):
+		"Recover time (ms)"
 		val = self.__bin[0x15] 
 		pcon2 = self.__bin[0x13]
 		mult = (pcon2 & ((1<<4)|(1<<5))) >> 4
 		return val * mult
 	def set_recover_time(self, ms):
+		"Recover time (ms)"
 		mult = ms >> 8
 		if mult > 3:
 			raise ACG_EEPROM_ValueError("Recover time out of range")
@@ -233,7 +291,11 @@ class eeprom:
 				d[ms % m] = m
 			keys = d.keys()
 			keys.sort()
-			mult = d[keys[0]]
+			try:
+				mult = d[keys[0]]
+			except:
+				raise ACG_EEPROM_ValueError( \
+						"Recover time out of range")
 			val = int(round(float(ms) / float(mult)))
 		else:
 			val = ms
@@ -244,8 +306,10 @@ class eeprom:
 		self.__set_byte(0x13, pcon2)
 
 	def get_afi(self):
+		"AFI"
 		return self.__bin[0x16]
 	def set_afi(self, ms):
+		"AFI"
 		self.__set_byte(0x16, ms)
 
 	def __get_timeslice(self, ofs):
@@ -258,38 +322,52 @@ class eeprom:
 		self.__set_byte(ofs, int(round(f / 0.3)))
 	
 	def get_tmo_1443a(self):
+		"Timeout ISO 1443-A"
 		return self.__get_timeslice(0x17)
 	def set_tmo_1443a(self, val):
+		"Timeout ISO 1443-A"
 		return self.__set_timeslice(0x17, val)
 
 	def get_tmo_1443b(self):
+		"Timeout ISO 1443-B"
 		return self.__get_timeslice(0x18)
 	def set_tmo_1443b(self, val):
+		"Timeout ISO 1443-B"
 		return self.__set_timeslice(0x18, val)
 
 	def get_tmo_sr176(self):
+		"Timeout SR-176"
 		return self.__get_timeslice(0x19)
 	def set_tmo_sr176(self, val):
+		"Timeout SR-176"
 		return self.__set_timeslice(0x19, val)
 
 	def get_tmo_15693(self):
+		"Timeout ISO 15693"
 		return self.__get_timeslice(0x1a)
 	def set_tmo_15693(self, val):
+		"Timeout ISO 15693"
 		return self.__set_timeslice(0x1a, val)
 
 	def get_auto_tmo(self):
+		"Auto timeout"
 		return not self.__get_bit(0x1b, PCON3_AUTO_TIMEOUT)
 	def set_auto_tmo(self, bit):
+		"Auto timeout"
 		self.__set_bit(0x1b, PCON3_AUTO_TIMEOUT, not bit)
 
 	def get_page_read(self):
+		"Page Read Mode"
 		return self.__get_bit(0x1b, PCON3_PAGE_READ)
-	def set_page_Read(self, bit):
+	def set_page_read(self, bit):
+		"Page Read Mode"
 		self.__set_bit(0x1b, PCON3_PAGE_READ, bit)
 
 	def get_ext_reqa(self):
+		"Ext. REQA"
 		return self.__get_bit(0x1b, PCON3_EXT_REQA)
 	def set_ext_reqa(self, bit):
+		"Ext. REQA"
 		self.__set_bit(0x1b, PCON3_EXT_REQA, bit)
 
 
@@ -307,48 +385,48 @@ class eeprom:
 		return result
 	def print_info(self):
 		fields = [
-			("Device ID", "0x%.8x", eeprom.get_dev_id),
-			("Admin Data", "0x%.8x", eeprom.get_admin_data),
-			("Station ID", "0x%.2x", eeprom.get_station_id),
-			("Baud rate", "%i", eeprom.get_baud_rate),
-			("Guard time (ms)", "%.3f", eeprom.get_guard),
-			("Auto start", "%r", eeprom.get_auto_start),
-			("Binary protocol", "%r", eeprom.get_binary),
-			("Multitag", "%r", eeprom.get_multitag),
-			("New serial mode", "%r", eeprom.get_new_serial_mode),
-			("Programmed LEDs", "%r", eeprom.get_program_led),
-			("Single shot mode", "%r", eeprom.get_single_shot),
-			("Ext. protocol", "%r", eeprom.get_ext_protocol),
-			("Ext. ID's", "%r", eeprom.get_ext_id),
-			("op: ISO 1443-A", "%r", eeprom.get_op_1443a),
-			("op: ISO 1443-B", "%r", eeprom.get_op_1443b),
-			("op: SR-176", "%r", eeprom.get_op_sr176),
-			("op: ICODE", "%r", eeprom.get_op_icode),
-			("op: ISO 15693", "%r", eeprom.get_op_15693),
-			("op: ICODE-EPC", "%r", eeprom.get_op_icode_epc),
-			("op: ICODE-UID", "%r", eeprom.get_op_icode_uid),
-			("Single shot (ms)", "%u", eeprom.get_single_shot_tmo),
-			("Multitag reset", "%r", eeprom.get_multitag_reset),
-			("Startup message", "%r", eeprom.get_startup_msg),
-			("Binary frames v2", "%r", eeprom.get_binary_frame2),
-			("Noisy environment", "%r", eeprom.get_noisy_env),
-			("ISO Anti-collision", "%r", eeprom.get_anti_collision),
-			("ISO Err handling", "%r", eeprom.get_err_handling),
-			("Reset time (ms)", "%u", eeprom.get_reset_time),
-			("Recover time (ms)", "%u", eeprom.get_recover_time),
-			("AFI", "%u", eeprom.get_afi),
-			("Timeout ISO 1443-A", "%.3f", eeprom.get_tmo_1443a),
-			("Timeout ISO 1443-B", "%.3f", eeprom.get_tmo_1443b),
-			("Timeout SR-176", "%.3f", eeprom.get_tmo_sr176),
-			("Timeout ISO 15693", "%.3f", eeprom.get_tmo_15693),
-			("Auto timeout", "%r", eeprom.get_auto_tmo),
-			("Page Read Mode", "%r", eeprom.get_page_read),
-			("Ext. REQA", "%r", eeprom.get_ext_reqa),
+			("0x%.8x", eeprom.get_dev_id),
+			("0x%.8x", eeprom.get_admin_data),
+			("0x%.2x", eeprom.get_station_id),
+			("%i", eeprom.get_baud_rate),
+			("%.3f", eeprom.get_guard),
+			("%r", eeprom.get_auto_start),
+			("%r", eeprom.get_binary),
+			("%r", eeprom.get_multitag),
+			("%r", eeprom.get_new_serial_mode),
+			("%r", eeprom.get_program_led),
+			("%r", eeprom.get_single_shot),
+			("%r", eeprom.get_ext_protocol),
+			("%r", eeprom.get_ext_id),
+			("%r", eeprom.get_op_1443a),
+			("%r", eeprom.get_op_1443b),
+			("%r", eeprom.get_op_sr176),
+			("%r", eeprom.get_op_icode),
+			("%r", eeprom.get_op_15693),
+			("%r", eeprom.get_op_icode_epc),
+			("%r", eeprom.get_op_icode_uid),
+			("%u", eeprom.get_single_shot_tmo),
+			("%r", eeprom.get_multitag_reset),
+			("%r", eeprom.get_startup_msg),
+			("%r", eeprom.get_binary_frame2),
+			("%r", eeprom.get_noisy_env),
+			("%r", eeprom.get_anti_collision),
+			("%r", eeprom.get_err_handling),
+			("%u", eeprom.get_reset_time),
+			("%u", eeprom.get_recover_time),
+			("%u", eeprom.get_afi),
+			("%.3f", eeprom.get_tmo_1443a),
+			("%.3f", eeprom.get_tmo_1443b),
+			("%.3f", eeprom.get_tmo_sr176),
+			("%.3f", eeprom.get_tmo_15693),
+			("%r", eeprom.get_auto_tmo),
+			("%r", eeprom.get_page_read),
+			("%r", eeprom.get_ext_reqa),
 		]
 
-		for (name, fmt, getter) in fields:
+		for (fmt, getter) in fields:
 			val = getter(self)
-			print ("  %-18s: %s"%(name, fmt))%val
+			print ("  %-18s: %s"%(getter.__doc__, fmt))%val
 
 		print "  Slack data:"
 		print self.__dump("".join(map(chr, self.__bin[0x1c:0x80])))
--- eepromtool.py
+++ eepromtool.py
@@ -1,19 +1,214 @@
 #!/usr/bin/python
 
-import acg
-import sys
+import sys, readline, acg
 
-def read_eeprom(bin):
-	try:
-		eeprom = acg.eeprom(bin)
-		eeprom.print_info()
-	except acg.ACG_Exception, e:
-		print e.msg
+class EEPROMShell(acg.eeprom):
+	def __help(self, arg):
+		"Print this help message"
+		print "Builtin Commands:"
+		keys = self.__builtins.keys()
+		keys.sort()
+		for k in keys:
+			v = self.__builtins[k]
+			print "  %s - %s"%(k, v.__doc__)
+		print
 
-if __name__ == '__main__':
-	for fn in sys.argv[1:]:
-		print "reading: %s"%fn
+		keys = self.__cmds.keys()
+		keys.sort()
+		print "EEPROM Variables:"
+		for k in keys:
+			(fmt, getter, cast, setter) = self.__cmds[k]
+			print "  %-16s : %s"%(k, getter.__doc__)
+		print
+
+	def __dump_info(self, arg):
+		"Print EEPROM settings"
+		self.print_info()
+
+	def __write(self, arg):
+		"Write EEPROM to file"
+		print "Writing to: %s"%self.__fn
+		f = open(self.__fn, 'w')
+		f.write(self.binary())
+		f.close()
+		print "Done"
+
+	def get_filename(self):
+		"Filename of the EEPROM (for writing)"
+		return self.__fn
+	def set_filename(self, fn):
+		"Filename of the EEPROM (for writing)"
+		try:
+			self.__fn = str(fn)
+		except ValueError:
+			raise acg.ACG_EEPROM_ValueError("Bad filename")
+
+	def __float(self, arg):
+		"Floating point"
+		return float(arg)
+	def __int(self, arg):
+		"Integer decimal/hexadecimal"
+		return int(arg, 0)
+	def __bool(self, arg):
+		"Boolean"
+		if arg.lower() in ["yes", "on", "true", "1"]:
+			return True
+		if arg.lower() in ["no", "off", "false", "0"]:
+			return False
+		raise ValueError
+
+	def __init__(self, fn):
+		self.__cmds = {
+			"dev_id": ("0x%.8x", self.get_dev_id,
+					self.__int, self.set_dev_id),
+			"admin_data": ("0x%.8x", self.get_admin_data,
+					self.__int, self.set_admin_data),
+			"station_id": ("0x%.2x", self.get_station_id,
+					self.__int, self.set_station_id),
+			"baud_rate": ("%i", self.get_baud_rate,
+					self.__int, self.set_baud_rate),
+			"guard_time": ("%.3f", self.get_guard,
+					self.__float, self.set_guard),
+			"auto_start": ("%r", self.get_auto_start,
+					self.__bool, self.set_auto_start),
+			"binary_proto": ("%r", self.get_binary,
+					self.__bool, self.set_binary),
+			"multitag": ("%r", self.get_multitag,
+					self.__bool, self.set_multitag),
+			"new_serial_mode": ("%r", self.get_new_serial_mode,
+					self.__bool, self.set_new_serial_mode),
+			"program_led": ("%r", self.get_program_led,
+					self.__bool, self.set_program_led),
+			"single_shot": ("%r", self.get_single_shot,
+					self.__bool, self.set_single_shot),
+			"ext_protocol": ("%r", self.get_ext_protocol,
+					self.__bool, self.set_ext_protocol),
+			"ext_id": ("%r", self.get_ext_id,
+					self.__bool, self.set_ext_id),
+			"op_1443a": ("%r", self.get_op_1443a,
+					self.__bool, self.set_op_1443a),
+			"op_1443b": ("%r", self.get_op_1443b,
+					self.__bool, self.set_op_1443b),
+			"op_sr176": ("%r", self.get_op_sr176,
+					self.__bool, self.set_op_sr176),
+			"op_icode": ("%r", self.get_op_icode,
+					self.__bool, self.set_op_icode),
+			"op_15693": ("%r", self.get_op_15693,
+					self.__bool, self.set_op_15693),
+			"op_icode_epc": ("%r", self.get_op_icode_epc,
+					self.__bool, self.set_op_icode_epc),
+			"op_icode_uid": ("%r", self.get_op_icode_uid,
+					self.__bool, self.set_op_icode_uid),
+			"single_shot_tmo": ("%u", self.get_single_shot_tmo,
+					self.__int, self.set_single_shot_tmo),
+			"multitag_reset": ("%r", self.get_multitag_reset,
+					self.__bool, self.set_multitag_reset),
+			"startup_msg": ("%r", self.get_startup_msg,
+					self.__bool, self.set_startup_msg),
+			"binary_frame2": ("%r", self.get_binary_frame2,
+					self.__bool, self.set_binary_frame2),
+			"noisy_env": ("%r", self.get_noisy_env,
+					self.__bool, self.set_noisy_env),
+			"anti_collision": ("%r", self.get_anti_collision,
+					self.__bool, self.set_anti_collision),
+			"err_handling": ("%r", self.get_err_handling,
+					self.__bool, self.set_err_handling),
+			"reset_time": ("%u", self.get_reset_time,
+					self.__int, self.set_reset_time),
+			"recover_time": ("%u", self.get_recover_time,
+					self.__int, self.set_recover_time),
+			"afi": ("%u", self.get_afi,
+					self.__int, self.set_afi),
+			"timeout_1443a": ("%.3f", self.get_tmo_1443a,
+					self.__float, self.set_tmo_1443a),
+			"timeout_1443b": ("%.3f", self.get_tmo_1443b,
+					self.__float, self.set_tmo_1443b),
+			"timeout_sr176": ("%.3f", self.get_tmo_sr176,
+					self.__float, self.set_tmo_sr176),
+			"timeout_15693": ("%.3f", self.get_tmo_15693,
+					self.__float, self.set_tmo_15693),
+			"auto_timeout": ("%r", self.get_auto_tmo,
+					self.__bool, self.set_auto_tmo),
+			"page_read": ("%r", self.get_page_read,
+					self.__bool, self.set_page_read),
+			"ext_reqa": ("%r", self.get_ext_reqa,
+					self.__bool, self.set_ext_reqa),
+			"filename": ("%s", self.get_filename,
+					str, self.set_filename),
+		}
+
+		self.__builtins = {
+			"?": self.__help,
+			"h": self.__help,
+			"p": self.__dump_info,
+			"w": self.__write,
+		}
+
+		self.__fn = fn
 		f = open(fn, 'r')
 		bin = f.read()
 		f.close()
-		read_eeprom(bin)
+		acg.eeprom.__init__(self, bin)
+
+	def __do_set(self, var, val):
+		if not self.__cmds.has_key(var):
+			print "%s: No such variable"%var
+			return
+		try:
+			(fmt, getter, cast, setter) = self.__cmds[var]
+			setter(cast(val))
+		except ValueError, e:
+			print "ERROR: setting %s: expected %s"%(var,
+							cast.__doc__)
+		except acg.ACG_EEPROM_ValueError, e:
+			print "ERROR: setting %s: %s"%(var, e.msg)
+		self.__do_get(var)
+		return
+
+	def __do_get(self, var):
+		if not self.__cmds.has_key(var):
+			print "%s: No such variable"%var
+			return
+
+		(fmt, getter, cast, setter) = self.__cmds[var]
+		print ("%s = %s"%(var, fmt))%getter()
+
+	def prompt(self):
+		return "eeprom> "
+
+	def command(self, cmd):
+		tok = cmd.split(None, 1)
+
+		if len(tok) == 2:
+			self.__do_set(tok[0], tok[1])
+			return
+		else:
+			tok.append(None)
+
+		if self.__builtins.has_key(tok[0]):
+			self.__builtins[tok[0]](tok[1])
+			return
+
+		self.__do_get(tok[0])
+
+if __name__ == '__main__':
+	eeprom_fn = sys.argv[1]
+	try:
+		shell = EEPROMShell(eeprom_fn)
+	except acg.ACG_Exception, e:
+		print e.msg
+
+	while True:
+		try:
+			line = raw_input(shell.prompt())
+		except EOFError:
+			print
+			break
+		except KeyboardInterrupt:
+			print
+			continue
+
+		try:
+			shell.command(line);
+		except KeyboardInterrupt:
+			continue
