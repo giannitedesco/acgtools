0a5af50092c05478ad54e4b1ad301deec16e5995
Gianni Tedesco <gianni@scaramanga.co.uk>
2010-02-28 13:05:32 +0000

    Implement bin2uint

--- acg/util.py
+++ acg/util.py
@@ -14,3 +14,12 @@ def bin2asc(binary):
 def bin2uint(binary):
 	"Convert big-endian binary integer to python integer"
 	return int("".join(map(lambda x:"%.2x"%x,binary)), 16)
+
+def uint2bin(uint, bytes=4):
+	bin = bytearray()
+	r = range(0, bytes << 3, 8)
+	r.reverse()
+	for i in r:
+		cur = (uint >> i) & 0xff
+		bin.append(chr(cur))
+	return bin
